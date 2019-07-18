b506b8711d17c1ce0bdbca1fbc4c07460d608624
Vttor <52765127+Vttor@users.noreply.github.com>
2019-07-17 21:08:14 -0300

    f4e8

---
+++ fase04/ex08/alfabetoMilitar.py
@@ -0,0 +1,14 @@
+def alfabetoMilitar(letra):
+    letra = letra.lower()
+    if letra == "a":
+        return "alpha"
+    elif letra == "e":
+        return "echo"
+    elif letra == "i":
+        return "india"
+    elif letra == "o":
+        return "oscar"
+    elif letra == "u":
+        return "uniform"
+    else:
+        return "false"
