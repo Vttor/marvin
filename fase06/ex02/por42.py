de805fc2f2822ec49c1dcd7025f98bad3cd9bb62
Vttor <52765127+Vttor@users.noreply.github.com>
2019-07-23 21:29:36 -0300

    f6e2

---
+++ fase06/ex02/por42.py
@@ -0,0 +1,16 @@
+def por42(num1,num2):
+    i = 0
+    j = 0
+    while(i == 0):
+        if (num1%42 != 0 and j != 2):
+            num1 = num1 + 1
+        else:
+            j = j + 1
+            if (j != 2):
+                num1 = num1 + 1
+        if (j == 2):
+            i = 1
+        if(num1 > num2):
+            num1 = False
+            i = 1
+    return num1
