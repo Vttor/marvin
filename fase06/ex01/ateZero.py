06f8902790a57969bb52a039ddd0643781f684cd
Vttor <52765127+Vttor@users.noreply.github.com>
2019-07-23 21:13:02 -0300

    f6e1

---
+++ fase06/ex01/ateZero.py
@@ -0,0 +1,10 @@
+def ateZero(num):
+    i=0
+    if(num<0):
+        while(num<=0):
+            print(num)
+            num = num + 1
+    else:
+        while(i<=num):
+            print(i)
+            i = i + 1
