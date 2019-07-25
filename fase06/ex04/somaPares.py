6e425f68c23a0e691192ce50a06378706ab70cc6
Vttor <52765127+Vttor@users.noreply.github.com>
2019-07-25 19:03:24 -0300

    f6e4

--- fase06/ex04/somaPares.py
+++ fase06/ex04/somaPares.py
@@ -1,13 +1,19 @@
 def somaPares(num1,num2):
-    i = round(num1,0)
-    soma = 0
-    while(i <= num2):
-        if(i % 2 == 0):
-            soma = soma + i
-            i = i + 1
+    def acabar(num1,num2):
+        from math import floor
+        i = floor(num1)
+        soma = 0
+        while(i <= num2):
+            if(i % 2 == 0):
+                soma = soma + i
+                i = i + 1
+            else:
+                i = i + 1
+        if(floor(num1) == num1 or floor(num1) % 2 != 0):
+            return (soma)
         else:
-            i = i + 1
-    if(round(num1,0) == num1 or round(num1,0) % 2 != 0):
-        return soma
+            return soma - floor(num1)
+    if(num1<num2):
+        return acabar(num1,num2)
     else:
-        return soma - round(num1,0)
+        return acabar(num2,num1)
