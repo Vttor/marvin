de138c0043faccbdefd9835e8cadad368af8347f
Vttor <52765127+Vttor@users.noreply.github.com>
2019-07-25 18:51:35 -0300

    f6e5

--- fase06/ex05/fizzbuzz.py
+++ fase06/ex05/fizzbuzz.py
@@ -1,12 +1,17 @@
 def fizzbuzz(num1,num2):
-    i = num1
-    while(i<=num2):
-        if(i%3 == 0 and i%5 == 0):
-            print(str(i) + " FizzBuzz")
-        elif(i%3 == 0):
-            print(str(i) + " Fizz")
-        elif(i%5 == 0):
-            print(str(i) + " Buzz")
-        else:
-            print(i)
-        i = i + 1
+    def acabar(num1,num2):
+        i = num1
+        while(i<=num2):
+            if(i%3 == 0 and i%5 == 0):
+                print(str(i) + " FizzBuzz")
+            elif(i%3 == 0):
+                print(str(i) + " Fizz")
+            elif(i%5 == 0):
+                print(str(i) + " Buzz")
+            else:
+                print(i)
+            i = i + 1
+    if(num1<num2):
+        acabar(num1,num2)
+    else:
+        acabar(num2,num1)
