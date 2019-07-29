e0817d38fb75f4de55a13e92834fe1f09c5544fd
Vttor <52765127+Vttor@users.noreply.github.com>
2019-07-29 19:04:25 -0300

    f6e6

--- fase06/ex06/booleanos.py
+++ fase06/ex06/booleanos.py
@@ -2,9 +2,12 @@ def booleanos(array):
     i = 0
     j = len(array)
     while(i < j):
-        if(array[i] == False or array[i] == True):
-            del array[i]
-            j = j - 1
+        if(array[i] == False or array[i] == True):        
+            if(str(array[i]) == "False" or str(array[i]) == "True"):
+                del array[i]
+                j = j - 1
+            else:
+                i = i + 1
         else:
             i = i + 1
     return array
