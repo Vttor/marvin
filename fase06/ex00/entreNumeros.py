0f8dfc6dc2f272d8f2ea4924e7bd633c6e9b2aa9
Vttor <52765127+Vttor@users.noreply.github.com>
2019-07-29 18:14:57 -0300

    f6e0

--- fase06/ex00/entreNumeros.py
+++ fase06/ex00/entreNumeros.py
@@ -1,4 +1,9 @@
-def entreNumeros(mi,ma):
-    while(mi <= ma):
-        print (mi)
-        mi = mi + 1
+def entreNumeros(x,y):    
+    def acabar(mi,ma):
+        while(mi <= ma):
+            print (mi)
+            mi = mi + 1
+    if (x<y):
+        acabar(x,y)
+    else:
+        acabar(y,x)
