5212d4ad45983baf54978c03781245afaeb87d25
Vttor <52765127+Vttor@users.noreply.github.com>
2019-08-01 19:33:58 -0300

    f7e5

--- fase07/ex05/organizarDatas.py
+++ fase07/ex05/organizarDatas.py
@@ -4,7 +4,6 @@ def organizarDatas(array):
     for i in range(0,qtd):
         array[i]["id"] = i
         arr.insert(i, array[i]["dataDeChegada"][6:] +array[i]["dataDeChegada"][:-8]+ array[i]["dataDeChegada"][3:5])
-    
     print(array)    
     print(arr)
 organizarDatas([{"planeta": "Plutão", "dataDeChegada": "02/21/2085"}, {"planeta": "Mercurio", "dataDeChegada": "05/13/2098"}])
