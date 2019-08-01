286a649dbac73b02c77498e6f527c54e1d7eab82
Vttor <52765127+Vttor@users.noreply.github.com>
2019-08-01 19:08:16 -0300

    f7e6

--- fase07/ex06/precoMedio.py
+++ fase07/ex06/precoMedio.py
@@ -10,4 +10,5 @@ def precoMedio(*x):
     for i in range(0,qtd):
         if (x[i]["preco"] ==  maiorPreco):
             print("O produto mais caro se chama " + x[i]["nome"])
+            break
     return round(soma/qtd,2)
