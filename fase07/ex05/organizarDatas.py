2524cebae89399093eba6a60ce5029b7237dff7a
Vttor <52765127+Vttor@users.noreply.github.com>
2019-08-01 20:00:13 -0300

    f7e5

--- fase07/ex05/organizarDatas.py
+++ fase07/ex05/organizarDatas.py
@@ -2,8 +2,7 @@ def organizarDatas(array):
     qtd = len(array)
     for i in range(0,qtd):
         array[i]["id"] = int(array[i]["dataDeChegada"][6:] +array[i]["dataDeChegada"][:-8]+ array[i]["dataDeChegada"][3:5])
-    arr = sorted(array, key = lambda k: k["id"])
+    array = sorted(array, key = lambda k: k["id"])
     for i in range(0,qtd):
-        del(arr[i]["id"])
-       
-organizarDatas([{"planeta": "Plutão", "dataDeChegada": "02/21/2100"}, {"planeta": "Mercurio", "dataDeChegada": "05/13/2098"}])
+        del(array[i]["id"])
+    return array
