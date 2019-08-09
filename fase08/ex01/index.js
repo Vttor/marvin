76bba0c3797532a04e1a4077d483f30df9d10c56
Vttor <52765127+Vttor@users.noreply.github.com>
2019-08-08 21:28:48 -0300

    f8e1

---
+++ fase08/ex01/index.js
@@ -0,0 +1,8 @@
+function clique(){
+    min=document.getElementById("min").value
+    max=document.getElementById("max").value
+    if(max<min){
+        alert("O valor minimo está menor que o máximo")
+    }
+    document.getElementById("texto").innerHTML="j"
+}
