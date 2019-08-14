75d964da0da75bd89b7f5e227e9184cb02e8038a
Vttor <52765127+Vttor@users.noreply.github.com>
2019-08-14 19:47:35 -0300

    f8e1js

--- fase08/ex01/index.js
+++ fase08/ex01/index.js
@@ -1,26 +1,21 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>FiizBuzz, de novo</title>
-    </head>
-    <body>
-    <div align="center">
-    <select name="min" id="min">
-        <option>-</option>
-        <option value="-5">-5</option>
-        <option value="0">0</option>
-        <option value="33">33</option>
-        <option value="42">42</option>
-    </select>
-    <select name="max" id="max">
-        <option>-</option>
-        <option value="3">3</option>
-        <option value="7">7</option>
-        <option value="81">81</option>
-        <option value="99">99</option>
-    </select>
-    <input type="button" value="OK" onclick=clique()>
-    <h1 id="texto"></h1>
-    </div>
-    </body>
-</html>
+function clique(){
+        min=document.getElementById("min").value
+        max=document.getElementById("max").value
+        var string=""
+    if (min!="-"&&max!="-"){
+        max=parseInt(max)
+        min=parseInt(min)
+        if(max<min){
+            alert("O valor minimo está menor que o máximo")
+        }
+        else{
+            for(var i=min;i<=max;i++){
+                string=string+i.toString()+"<br>"
+            }
+            document.getElementById("texto").innerHTML=string
+        }
+    }
+    else{
+        alert("Escolha os dois valores")
+    }
+}
